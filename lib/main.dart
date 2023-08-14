80b0c9e1563dd157920cd6619a559901459840c7
Leofelixpro <118075509+Leofelixpro@users.noreply.github.com>
2023-08-14 09:22:48 -0300

    Nome do app
    estava com o nome junto e com letra minuscula

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   Future<void> _loadDataAndNavigate() async {
-    await Future.delayed(const Duration(seconds: 5));
+    await Future.delayed(const Duration(seconds: 3));
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => HomePageWidget()),

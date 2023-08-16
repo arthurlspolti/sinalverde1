df3828ca065b1f6f12abc0eccb3b4589811a5084
Arthur <arthurlspolti2@gmail.com>
2023-08-16 17:38:46 -0300

    toque aplicativo ligado

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 import '../widgets/audio_button_home.dart';
 import 'my_touch_page.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class HomePageWidget extends StatelessWidget {
+class HomePageWidget extends StatefulWidget {
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  AudioPlayer _audioPlayer = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    _playAudio(); // Chama a função para reproduzir o áudio
+  }
+
+  Future<void> _playAudio() async {
+    await _audioPlayer.play(AssetSource('toque.mp3'));
+    await _audioPlayer.setVolume(1);
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return GestureDetector(

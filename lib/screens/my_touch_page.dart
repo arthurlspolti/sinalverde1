1b87da8c4fc10ace5979382bd04f9af71a909fa7
Leonardo Felix Apolinario <leofelixap34@gmail.com>
2023-08-17 16:32:18 -0300

    time certo

--- lib/screens/my_touch_page.dart
+++ lib/screens/my_touch_page.dart
@@ -15,7 +15,7 @@ class _MyTouchPageState extends State<MyTouchPage> {
   void initState() {
     super.initState();
 
-    Timer(Duration(seconds: 6), () {
+    Timer(Duration(seconds: 8), () {
       setState(() {
         _isButtonDisabled = false;
       });

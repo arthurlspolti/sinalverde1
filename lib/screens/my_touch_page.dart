e0442aca677a0e86072c589f74a83dc8ec0ae3fb
wesley <wesleyaleixo0950@gmail.com>
2023-08-17 16:24:52 -0300

    Fix

--- lib/screens/my_touch_page.dart
+++ lib/screens/my_touch_page.dart
@@ -1,36 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'second_page.dart';
 
-class MyTouchPage extends StatelessWidget {
+class MyTouchPage extends StatefulWidget {
+  @override
+  _MyTouchPageState createState() => _MyTouchPageState();
+}
+
+class _MyTouchPageState extends State<MyTouchPage> {
+  bool _isButtonDisabled = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 6), () {
+      setState(() {
+        _isButtonDisabled = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => SecondPage()),
-        );
-      },
-      child: Scaffold(
-        backgroundColor: Color(0xFF247BA0),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
+    return Scaffold(
+      backgroundColor: Color(0xFF247BA0),
+      body: Center(
+        child: GestureDetector(
+          onTap: _isButtonDisabled
+              ? null
+              : () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => SecondPage()),
                   );
                 },
-                child: Icon(
-                  Icons.arrow_forward,
-                  color: Colors.white,
-                  size: 120,
-                ),
+          child: Semantics(
+            label: "Avançar para a próxima página",
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Icon(
+                Icons.arrow_forward,
+                color: Colors.white,
+                size: 120000,
               ),
-            ],
+            ),
           ),
         ),
       ),

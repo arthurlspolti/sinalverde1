d24551c54c61459c7fcdfa875adc0990197ff577
Arthur <arthurlspolti2@gmail.com>
2023-08-22 15:08:35 -0300

    testo

--- lib/screens/second_page.dart
+++ lib/screens/second_page.dart
@@ -1,51 +1,26 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 import 'package:http/http.dart' as http;
 
-class SecondPage extends StatefulWidget {
-  @override
-  _SecondPageState createState() => _SecondPageState();
+void main() {
+  runApp(SecondPage());
 }
 
-class _SecondPageState extends State<SecondPage> {
-  AudioPlayer _audioPlayer = AudioPlayer();
-  Color _circleColor = Colors.green;
-  Timer? _timer;
+
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPage createState() => _SecondPage();
+}
+
+class _SecondPage extends State<SecondPage> {
+  Color _circleColor = Colors.grey;
+  String _semaforoStatus = 'Carregando...';
 
   @override
   void initState() {
     super.initState();
-    _initWifi();
-  }
-
-  String semaforoStatus = 'Carregando...';
-
-  Future<void> fetchSemaforoStatus() async {
-    final response = await http.get(Uri.parse('http://192.168.4.1/status'));
-
-    if (response.statusCode == 200) {
-      setState(() {
-         _circleColor = Colors.red;
-        _playAudio('sinalvermelho.mp3');
-      });
-    } else if (response.statusCode == 100) {
-      setState(() {
-        _circleColor = Colors.green;
-        _playAudio('sinalverde.mp3');
-      });
-    }
-  }
-
-  Future<void> _initWifi() async {
-    if (!await WiFiForIoTPlugin.isConnected()) {
-      bool isWifiEnabled = await WiFiForIoTPlugin.isEnabled();
-      if (!isWifiEnabled) {
-        await WiFiForIoTPlugin.setEnabled(true);
-      }
-      await _connectToWifi();
-    }
+    _connectToWifi();
+    _startCheckingStatusLoop();
   }
 
   Future<void> _connectToWifi() async {
@@ -58,10 +33,7 @@ class _SecondPageState extends State<SecondPage> {
       );
 
       if (isConnected) {
-        setState(() {
-          _circleColor = Colors.green;
-          _playAudio('toque.mp3');
-        });
+        // Do something after successful connection
       } else {
         print('Failed to connect to Wi-Fi');
       }
@@ -70,70 +42,61 @@ class _SecondPageState extends State<SecondPage> {
     }
   }
 
-  Future<void> _playAudio(String assetPath) async {
-    await _audioPlayer.stop(); // Stop previous playback
-    await _audioPlayer.play(AssetSource(assetPath));
-    await _audioPlayer.setVolume(1);
+  Future<void> _startCheckingStatusLoop() async {
+    while (true) {
+      await fetchSemaforoStatus();
+      await Future.delayed(Duration(milliseconds: 100));
+    }
   }
 
+  Future<void> fetchSemaforoStatus() async {
+  try {
+    final response = await http.get(Uri.parse('http://192.168.4.1/status'));
 
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    _timer?.cancel();
-    super.dispose();
+    if (response.statusCode == 200) {
+      setState(() {
+        _semaforoStatus = response.body;
+      });
+
+      if (_semaforoStatus == 'Sinal Verde') {
+        setState(() {
+          _circleColor = Colors.green;
+          // Execute actions for 'Sinal Verde'
+        });
+      } else if (_semaforoStatus == 'Sinal Vermelho') {
+        setState(() {
+          _circleColor = Colors.red;
+          // Execute actions for 'Sinal Vermelho'
+        });
+      }
+    } else {
+      print('Failed to fetch semáforo status');
+    }
+  } catch (e) {
+    print('Error fetching semáforo status: $e');
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF247BA0),
       appBar: AppBar(
-        backgroundColor: Color(0xFF13293D),
+        title: Text('Wi-Fi Connection Example'),
       ),
-      body: SafeArea(
-        top: true,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.3,
-                height: MediaQuery.of(context).size.width * 0.3,
-                decoration: BoxDecoration(
-                  color: _circleColor,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Text(
-                    _circleColor == Colors.green ? 'Avançar' : 'Parar2',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontFamily: 'verdana',
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  _simulateArduinoSignal();
-                },
-                child: Text('Simular Sinal Arduino'),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: _circleColor,
+            ),
+            SizedBox(height: 20),
+            Text('Status do Semáforo: $_semaforoStatus'),
+          ],
         ),
       ),
     );
   }
-
-  void _simulateArduinoSignal() async {
-    await _initWifi();
-  }
-}
-
-void main() {
-  runApp(MaterialApp(home: SecondPage()));
 }
